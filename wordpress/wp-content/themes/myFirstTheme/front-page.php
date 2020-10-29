155b1763c4a38344e7b61d17204975d55f60014f
fwauters <fwauters13@gmail.com>
2020-10-29 14:28:36 +0100

    update

--- wordpress/wp-content/themes/myFirstTheme/front-page.php
+++ wordpress/wp-content/themes/myFirstTheme/front-page.php
@@ -1,11 +1,12 @@
 <?php get_header() ?>
 
 <div class="jumbotron bg-light mx-2">
-    <h1 class="display-4">Hello, world!</h1>
-    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <h1 class="display-4">Good Horror !</h1>
     <hr class="my-4">
-    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-    <a class="btn btn-dark btn-lg" href="#" role="button">Learn more</a>
+    <p class="lead">This is a simple catalogue of my favorite horror movies.</p>
+    <hr class="my-4">
+    <p class="mb-5">This is juste a place-holder text to test the jumbotron. PS: the button doesn't work yet ^^</p>
+    <a class="btn btn-dark btn-lg" href="#" role="button">See the catalogue</a>
 </div>
 
 <?php get_footer() ?>
